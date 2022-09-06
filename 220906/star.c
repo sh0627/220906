6ca2d379dd83d02a027cbe08fa7d9c0105f7ad61
sh0627 <dlse0973@naver.com>
2022-09-06 09:57:50 +0900

    빈 다이아몬드 찍기

--- 220906/star.c
+++ 220906/star.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	int input = 0;
+	int num = 0;
 
-	scanf_s("%d", &input);
+	printf("1부터 100 사이의 수를 입력하세요\n");
+	scanf_s("%d", &num);
+	printf("\n");
 
-	for (int i = 0; i < input; i++)
-	{
-		printf("**");
+	if (num < 1 || num > 100) {
+		printf("1부터 100 사이의 수가 아닙니다.\n");
+		return 0;
 	}
 
+	for (int i = 0; i < num; i++) {
+		for (int j = num - i; j > 0; j--) {
+			printf("*");
+		}
+		for (int k = 0; k < i * 2; k++) {
+			printf(" ");
+		}
+		for (int n = 0; n < num - i; n++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j <= i ; j++) {
+			printf("*");
+		}
+		for (int k = 0; k < (2 * num) - (2 * (i + 1)); k++) {
+			printf(" ");
+		}
+		for (int n = 0; n <= i; n++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
